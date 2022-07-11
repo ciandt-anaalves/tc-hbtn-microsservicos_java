f539428a8839beb856b0479c58e4108e3745b05e
tc-anaalves <anaalves@ciandt.com>
2022-07-11 16:40:56 -0300

    Construindo uma API REST com Spring Boot Security - MEGA SENA API

---
+++ 0x05/MegaSenaAPI/src/main/java/com/song/MegaSenaAPI/MegaSenaApiApplication.java
@@ -0,0 +1,13 @@
+package com.song.MegaSenaAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegaSenaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MegaSenaApiApplication.class, args);
+	}
+
+}
---
+++ 0x05/MegaSenaAPI/src/main/java/com/song/MegaSenaAPI/MegaSenaController.java
@@ -0,0 +1,36 @@
+package com.song.MegaSenaAPI;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.*;
+
+@RestController
+@RequestMapping(value = "/megasena")
+public class MegaSenaController {
+
+    @GetMapping("/simpleMessageWelcome")
+    public String mensagemBoasVindas() {
+        return "BEM VINDO A API REST DA LOTERIA MEGA SENA USANDO SPRING SECURITY !!!";
+    }
+
+    @GetMapping("/getNumbers")
+    public List<Integer> numerosMegaSena() {
+        return generateListWithRandomIntegerNumbers(6, 1, 60);
+    }
+
+    private List<Integer> generateListWithRandomIntegerNumbers(int size, int lowerBound, int upperBound) {
+        Set<Integer> setRandom = new LinkedHashSet<Integer>();
+        while (setRandom.size() < size) {
+            setRandom.add((int) (Math.random() * (upperBound + 1 - lowerBound) + lowerBound));
+        }
+
+        List<Integer> result = new ArrayList<Integer>(setRandom);
+
+        Collections.sort(result);
+
+        return result;
+    }
+
+}
